8c0b4f5d7a45b620bef7295b4e6ad5b3ff94c07d
P202U <156369604+P202U@users.noreply.github.com.>
2024-12-15 11:42:43 +0530

    Correct deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import Header from './components/Header';
 import Search from './components/Search';
 // import List from './components/List';
 import Recipe from './components/Recipe';
+import { SpeedInsights } from '@vercel/speed-insights/react'; // Import here
 
 export default function App() {
   return (
-    <div className="conatiner mx-auto h-[90vh] w-full max-w-3xl rounded-lg bg-blue-400 p-2 text-white">
+    <div className="container mx-auto h-[90vh] w-full max-w-3xl rounded-lg bg-blue-400 p-2 text-white">
       <div className="h-full w-full space-y-14 overflow-auto rounded-lg bg-blue-500">
         <Header />
         <Search />
         {/* <List /> */}
         <Recipe />
+        <SpeedInsights />
       </div>
     </div>
   );
